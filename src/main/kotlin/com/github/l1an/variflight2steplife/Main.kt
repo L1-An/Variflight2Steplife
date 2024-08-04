ed6803ec55e33ea99dc79daf01521c89e3f6a98f
L1-An <shiyu46@icloud.com>
2024-08-04 22:02:14 +0800

    更新校验功能 修复原来经程序处理的csv无法使用的问题

--- src/main/kotlin/com/github/l1an/variflight2steplife/Main.kt
+++ src/main/kotlin/com/github/l1an/variflight2steplife/Main.kt
@@ -5,10 +5,27 @@ import com.opencsv.CSVWriter
 import java.io.FileReader
 import java.io.FileWriter
 import java.io.IOException
+import java.text.DecimalFormat
+import java.util.Scanner
 
 fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("请选择操作:")
+    println("1. 转换 csv 文件")
+    println("2. 检测 output 和手动 output 文件")
+
+    when (scanner.nextLine().trim()) {
+        "1" -> convertCSV()
+        "2" -> checkCSV()
+        else -> println("无效选择, 请输入1 或 2")
+    }
+}
+
+fun convertCSV() {
     val inputFilePath = "src/main/resources/input.csv"
     val outputFilePath = "src/main/resources/output.csv"
+    val decimalFormat = DecimalFormat("#.#######")
 
     try {
         CSVReader(FileReader(inputFilePath)).use {
@@ -67,10 +84,10 @@ fun main() {
                     while (reader.readNext().also { nextLine = it } != null) {
                         // 处理数据
                         val dataTime = nextLine!![0]
-                        val longitude = nextLine!![7]
-                        val latitude = nextLine!![8]
-                        val speed = nextLine!![5]
-                        val altitude = nextLine!![4]
+                        val longitude = decimalFormat.format(nextLine!![7].toDouble())
+                        val latitude = decimalFormat.format(nextLine!![8].toDouble())
+                        val speed = decimalFormat.format(nextLine!![5].toDouble())
+                        val altitude = decimalFormat.format(nextLine!![4].toDouble())
                         val outputLine = arrayOf(
                             dataTime, "1", longitude, latitude, "0", "14", speed, "0", "0", "0", altitude
                         )
@@ -79,6 +96,43 @@ fun main() {
                 }
             }
             println("文件处理完成，共处理 $totalLines 行数据")
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
+
+fun checkCSV() {
+    val manualFilePath = "src/main/resources/manual_output.csv"
+    val programFilePath = "src/main/resources/output.csv"
+
+    try {
+        CSVReader(FileReader(manualFilePath)).use { manualReader ->
+            CSVReader(FileReader(programFilePath)).use { programReader ->
+                var manualLine: Array<String>?
+                var programLine: Array<String>?
+
+                var lineNumber = 0
+                var discrepanciesFound = false
+
+                while (manualReader.readNext().also { manualLine = it } != null) {
+                    lineNumber++
+                    programLine = programReader.readNext()
+
+                    if (programLine == null || !manualLine.contentEquals(programLine)) {
+                        println("Discrepancy found at line $lineNumber")
+                        println("Manual:  ${manualLine?.joinToString()}")
+                        println("Program: ${programLine?.joinToString()}")
+                        discrepanciesFound = true
+                    }
+                }
+
+                if (!discrepanciesFound) {
+                    println("Files match perfectly!")
+                } else {
+                    println("Discrepancies found in the files.")
+                }
+            }
         }
     } catch (e: IOException) {
         e.printStackTrace()
